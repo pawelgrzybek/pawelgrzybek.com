42e17294543ee0294000460c85393db41f67d727
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2022-12-16 13:13:03 +0000

    fix form

--- static/comments.js
+++ static/comments.js
@@ -3,7 +3,6 @@ const LOCAL_STORAGE_KEY = "comments-details";
 const SELECTOR_FORM = ".js-form";
 const SELECTOR_FORM_NAME = ".js-form-name";
 const SELECTOR_FORM_WEBSITE = ".js-form-website";
-const SELECTOR_FORM_TWITTER = ".js-form-twitter";
 const SELECTOR_FORM_GITHUB = ".js-form-github";
 const SELECTOR_FORM_SAVE_DATA = ".js-form-save-data";
 const SELECTOR_REPLY_BUTTON = ".js-comments__reply";
@@ -16,16 +15,13 @@ const CLASS_FORM_CONFIRMATION_HIDDEN = "form__confirmation--hidden";
 
 const savedData = localStorage.getItem(LOCAL_STORAGE_KEY);
 if (savedData) {
-  const { name, website, twitter, github, save } = JSON.parse(savedData);
+  const { name, website, github, save } = JSON.parse(savedData);
   document
     .querySelectorAll(SELECTOR_FORM_NAME)
     .forEach((i) => (i.value = name));
   document
     .querySelectorAll(SELECTOR_FORM_WEBSITE)
     .forEach((i) => (i.value = website));
-  document
-    .querySelectorAll(SELECTOR_FORM_TWITTER)
-    .forEach((i) => (i.value = twitter));
   document
     .querySelectorAll(SELECTOR_FORM_GITHUB)
     .forEach((i) => (i.value = github));
@@ -49,7 +45,6 @@ async function handlerSubmitForm(event) {
 
   const formFieldName = this.querySelector('input[name="name"]');
   const formFieldWebsite = this.querySelector('input[name="website"]');
-  const formFieldTwitter = this.querySelector('input[name="twitter"]');
   const formFieldGitHub = this.querySelector('input[name="github"]');
   const formFieldComment = this.querySelector('textarea[name="comment"]');
   const formFieldParent = this.querySelector('input[name="parent"]');
@@ -66,7 +61,6 @@ async function handlerSubmitForm(event) {
         body: JSON.stringify({
           name: formFieldName.value,
           website: formFieldWebsite.value,
-          twitter: formFieldTwitter.value,
           github: formFieldGitHub.value,
           comment: formFieldComment.value,
           parent: formFieldParent.value,
@@ -84,7 +78,6 @@ async function handlerSubmitForm(event) {
       JSON.stringify({
         name: formFieldName.value,
         website: formFieldWebsite.value,
-        twitter: formFieldTwitter.value,
         github: formFieldGitHub.value,
         save: this.querySelector('input[name="save"]').checked,
       })
@@ -93,7 +86,6 @@ async function handlerSubmitForm(event) {
     [
       formFieldName,
       formFieldWebsite,
-      formFieldTwitter,
       formFieldGitHub,
       formFieldComment,
     ].forEach((i) => (i.value = ""));
