d61d67e1a29058e9ed6d4cb193ebe94f1b46489a
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2023-10-26 08:46:40 +0100

    Replace radio buttons with buttons

--- themes/pawelgrzybek/static/theme.js
+++ themes/pawelgrzybek/static/theme.js
@@ -1,16 +1,23 @@
-const themePickerInputs = document.querySelectorAll(".js-theme input");
+const themePickerButtons = document.querySelectorAll(".js-theme button");
 
 const userTheme = localStorage.getItem("theme");
 
 if (userTheme) {
-  [...themePickerInputs].find(
-    ({ value }) => value === userTheme
-  ).checked = true;
+  themePickerButtons.forEach((button) =>
+    button.setAttribute("aria-pressed", "false")
+  );
+  [...themePickerButtons]
+    .find(({ value }) => value === userTheme)
+    .setAttribute("aria-pressed", "true");
   document.documentElement.setAttribute("data-theme", userTheme);
 }
 
-themePickerInputs.forEach((input) => {
-  input.addEventListener("change", (e) => {
+themePickerButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    themePickerButtons.forEach((button) =>
+      button.setAttribute("aria-pressed", "false")
+    );
+    e.target.setAttribute("aria-pressed", "true");
     const theme = e.target.value;
     document.documentElement.setAttribute("data-theme", theme);
 
