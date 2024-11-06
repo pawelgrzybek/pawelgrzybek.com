0ec7108f7de6106b6f6a97e1ce19ca677edc218a
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-11-06 09:33:44 +0000

    change the way how theming works

--- themes/pawelgrzybek/static/theme.js
+++ themes/pawelgrzybek/static/theme.js
@@ -4,25 +4,39 @@ const userTheme = localStorage.getItem("theme");
 
 if (userTheme) {
   themePickerButtons.forEach((button) =>
-    button.setAttribute("aria-pressed", "false")
+    button.setAttribute("aria-pressed", "false"),
   );
   [...themePickerButtons]
     .find(({ value }) => value === userTheme)
     .setAttribute("aria-pressed", "true");
-  document.documentElement.setAttribute("data-theme", userTheme);
+  document
+    .querySelector('meta[name="color-scheme"]')
+    .setAttribute("content", userTheme);
+  document.querySelector("html").style.setProperty("color-scheme", userTheme);
 }
 
 themePickerButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
     themePickerButtons.forEach((button) =>
-      button.setAttribute("aria-pressed", "false")
+      button.setAttribute("aria-pressed", "false"),
     );
     e.target.setAttribute("aria-pressed", "true");
     const theme = e.target.value;
-    document.documentElement.setAttribute("data-theme", theme);
 
-    theme === "system"
-      ? localStorage.removeItem("theme")
-      : localStorage.setItem("theme", theme);
+    if (theme === "system") {
+      localStorage.removeItem("theme");
+      document
+        .querySelector('meta[name="color-scheme"]')
+        .setAttribute("content", "light dark");
+      document
+        .querySelector("html")
+        .style.setProperty("color-scheme", "light dark");
+    } else {
+      localStorage.setItem("theme", theme);
+      document
+        .querySelector('meta[name="color-scheme"]')
+        .setAttribute("content", theme);
+      document.querySelector("html").style.setProperty("color-scheme", theme);
+    }
   });
 });
